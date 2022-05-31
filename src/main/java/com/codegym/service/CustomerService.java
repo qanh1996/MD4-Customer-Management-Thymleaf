d949cc592679b2759117ef7ae5902b207deb0452
unknown <phamthulien1975@gmail.com>
2022-05-31 17:26:15 +0700

    up

---
+++ src/main/java/com/codegym/service/CustomerService.java
@@ -0,0 +1,45 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService implements ICustomerService{
+    private static final Map<Integer,Customer> customers;
+    static {
+        customers=new HashMap<>();
+        customers.put(1,new Customer(1,"Long","long@gmail.com","Ha Noi"));
+        customers.put(2,new Customer(2,"Hung","hung@gmail.com","Phu Tho"));
+        customers.put(3,new Customer(3,"Dung","dung@gmail.com","Tuyen Quang"));
+        customers.put(4,new Customer(4,"Hung","hung1@gmail.com","Thai Binh"));
+        customers.put(5,new Customer(5,"Phong","phong@gmail.com","Ha Noi"));
+        customers.put(6,new Customer(6,"Quoc Anh","quocanh@gmail.com","Ha Noi"));
+    }
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers.values());
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customers.put(customer.getId(),customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return customers.get(id);
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        customers.put(id,customer);
+    }
+
+    @Override
+    public void remove(int id) {
+        customers.remove(id);
+    }
+}
